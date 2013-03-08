ccbc2fe717c738fe263760ed9e9e6be2d3503fef
Heinz Mauelshagen <heinzm@redhat.com>
2013-03-08 15:29:18 +0100

    dm cache: introduce dm-cache-policy-background.c
    Add a stackable cache replacement policy module allowing writebacks of
    a configurable amount of dirty cache blocks. Hence those blocks don't
    need to get written back on eviction any more thus gaining performance

--- drivers/md/dm-cache-policy-cleaner.c
+++ drivers/md/dm-cache-policy-cleaner.c
@@ -12,7 +12,6 @@
 #include <linux/hash.h>
 #include <linux/module.h>
 #include <linux/slab.h>
-#include <linux/vmalloc.h>
 
 /*----------------------------------------------------------------*/
 
@@ -26,7 +25,6 @@ struct wb_cache_entry {
 	dm_oblock_t oblock;
 	dm_cblock_t cblock;
 	bool dirty:1;
-	bool pending:1;
 };
 
 struct hash {
@@ -71,9 +69,7 @@ static struct policy *to_policy(struct dm_cache_policy *p)
 static struct list_head *list_pop(struct list_head *q)
 {
 	struct list_head *r = q->next;
-
 	list_del(r);
-
 	return r;
 }
 
@@ -220,13 +216,15 @@ static int wb_lookup(struct dm_cache_policy *pe, dm_oblock_t oblock, dm_cblock_t
 	return r;
 }
 
-static void __set_clear_dirty(struct dm_cache_policy *pe, dm_oblock_t oblock, bool set)
+static int __set_clear_dirty(struct dm_cache_policy *pe, dm_oblock_t oblock, bool set)
 {
+	int r;
 	struct policy *p = to_policy(pe);
 	struct wb_cache_entry *e;
 
 	e = lookup_cache_entry(p, oblock);
 	BUG_ON(!e);
+	r = e->dirty ? 1 : 0;
 
 	if (set) {
 		if (!e->dirty) {
@@ -236,31 +234,38 @@ static void __set_clear_dirty(struct dm_cache_policy *pe, dm_oblock_t oblock, bo
 
 	} else {
 		if (e->dirty) {
-			e->pending = false;
 			e->dirty = false;
 			list_move(&e->list, &p->clean);
 		}
 	}
+
+	return r;
 }
 
-static void wb_set_dirty(struct dm_cache_policy *pe, dm_oblock_t oblock)
+static int wb_set_dirty(struct dm_cache_policy *pe, dm_oblock_t oblock)
 {
+	int r;
 	struct policy *p = to_policy(pe);
 	unsigned long flags;
 
 	spin_lock_irqsave(&p->lock, flags);
-	__set_clear_dirty(pe, oblock, true);
+	r = __set_clear_dirty(pe, oblock, true);
 	spin_unlock_irqrestore(&p->lock, flags);
+
+	return r;
 }
 
-static void wb_clear_dirty(struct dm_cache_policy *pe, dm_oblock_t oblock)
+static int wb_clear_dirty(struct dm_cache_policy *pe, dm_oblock_t oblock)
 {
+	int r;
 	struct policy *p = to_policy(pe);
 	unsigned long flags;
 
 	spin_lock_irqsave(&p->lock, flags);
-	__set_clear_dirty(pe, oblock, false);
+	r = __set_clear_dirty(pe, oblock, false);
 	spin_unlock_irqrestore(&p->lock, flags);
+
+	return r;
 }
 
 static void add_cache_entry(struct policy *p, struct wb_cache_entry *e)
@@ -396,6 +401,7 @@ static void init_policy_functions(struct policy *p)
 	p->policy.walk_mappings = NULL;
 	p->policy.remove_mapping = wb_remove_mapping;
 	p->policy.writeback_work = wb_writeback_work;
+	p->policy.next_dirty_block = NULL;
 	p->policy.force_mapping = wb_force_mapping;
 	p->policy.residency = wb_residency;
 	p->policy.tick = NULL;
@@ -403,7 +409,7 @@ static void init_policy_functions(struct policy *p)
 
 static struct dm_cache_policy *wb_create(dm_cblock_t cache_size,
 					 sector_t origin_size,
-					 sector_t cache_block_size)
+					 sector_t block_size)
 {
 	int r;
 	struct policy *p = kzalloc(sizeof(*p), GFP_KERNEL);
