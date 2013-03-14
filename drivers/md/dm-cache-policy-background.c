5b01c3250b89da1d893045dc0ca64a2f2e5f1866
Heinz Mauelshagen <heinzm@redhat.com>
2013-03-14 11:47:23 +0100

    dm cache: dm-cache-policy-background.c: start fixing threshold processing

--- drivers/md/dm-cache-policy-background.c
+++ drivers/md/dm-cache-policy-background.c
@@ -6,14 +6,9 @@
  * This file is released under the GPL.
  */
 
-#include "dm-cache-policy.h"
 #include "dm-cache-policy-internal.h"
-#include "dm.h"
 
 #include <linux/module.h>
-#include <linux/moduleparam.h>
-
-#define	LLU	long long unsigned
 
 /*----------------------------------------------------------------*/
 
@@ -22,6 +17,7 @@ struct policy {
 	struct dm_cache_policy *real_policy;
 
 	dm_cblock_t threshold;
+	atomic_t nr_dirty;
 
 	dm_cblock_t cache_blocks;
 	sector_t origin_sectors;
@@ -54,47 +50,62 @@ static int background_map(struct dm_cache_policy *pe, dm_oblock_t oblock,
 {
 	struct policy *p = to_policy(pe);
 
-	return p->real_policy ? policy_map(p->real_policy, oblock, can_block, can_migrate,
-					   discarded_oblock, bio, result) : 0;
+	if (p->real_policy)
+		return policy_map(p->real_policy, oblock, can_block, can_migrate,
+				  discarded_oblock, bio, result);
+	else {
+		result->op = POLICY_MISS;
+		return 0;
+	}
 }
 
 static int background_lookup(struct dm_cache_policy *pe, dm_oblock_t oblock, dm_cblock_t *cblock)
 {
 	struct policy *p = to_policy(pe);
 
-	return p->real_policy ? policy_lookup(p->real_policy, oblock, cblock) : 0;
+	return p->real_policy ? policy_lookup(p->real_policy, oblock, cblock) : -ENOENT;
+}
+
+static int _set_clear_dirty(struct dm_cache_policy *pe,
+			     int (*fn)(struct dm_cache_policy*, dm_oblock_t),
+			     dm_oblock_t oblock)
+{
+	int r;
+	struct policy *p = to_policy(pe);
+
+	if (p->real_policy) {
+		r = fn(p->real_policy, oblock);
+		if (r > 0) {
+			int nr = atomic_read(&p->nr_dirty);
+
+			if (fn == policy_set_dirty) {
+				if (nr == from_cblock(p->cache_blocks))
+					pr_alert("nr_dirty already max!\n");
+				else
+					atomic_inc(&p->nr_dirty);
+
+			} else {
+				if (nr)
+					atomic_dec(&p->nr_dirty);
+				else
+					pr_alert("nr_dirty already 0!\n");
+			}
+		}
+
+	} else
+		r = -EOPNOTSUPP;
+
+	return r;
 }
 
 static int background_set_dirty(struct dm_cache_policy *pe, dm_oblock_t oblock)
 {
-	int r = -EOPNOTSUPP;
-	struct policy *p = to_policy(pe);
-
-	if (p->real_policy) {
-		r = policy_set_dirty(p->real_policy, oblock);
-		if (r < 0) {
-			BUG_ON(r != -ENOENT);
-			r = 0;
-		}
-	}
-
-	return r;
+	return _set_clear_dirty(pe, policy_set_dirty, oblock);
 }
 
 static int background_clear_dirty(struct dm_cache_policy *pe, dm_oblock_t oblock)
 {
-	int r = -EOPNOTSUPP;
-	struct policy *p = to_policy(pe);
-
-	if (p->real_policy) {
-		r = policy_clear_dirty(p->real_policy, oblock);
-		if (r < 0) {
-			BUG_ON(r != -ENOENT);
-			r = 0;
-		}
-	}
-
-	return r;
+	return _set_clear_dirty(pe, policy_clear_dirty, oblock);
 }
 
 static int background_load_mapping(struct dm_cache_policy *pe,
@@ -134,14 +145,28 @@ static void background_force_mapping(struct dm_cache_policy *pe,
 static int background_writeback_work(struct dm_cache_policy *pe,
 				     dm_oblock_t *oblock, dm_cblock_t *cblock)
 {
-	return policy_next_dirty_block(to_policy(pe)->real_policy, oblock, cblock);
+	int r;
+	struct policy *p = to_policy(pe);
+
+	if (from_cblock(p->cache_blocks) - atomic_read(&p->nr_dirty) > from_cblock(p->threshold))
+		return -ENOENT;
+
+	r =  policy_next_dirty_block(p->real_policy, oblock, cblock);
+	if (!r) {
+		if (atomic_read(&p->nr_dirty))
+			atomic_dec(&p->nr_dirty);
+		else
+			pr_alert("nr_dirty already 0!\n");
+	}
+
+	return r;
 }
 
 static dm_cblock_t background_residency(struct dm_cache_policy *pe)
 {
 	struct policy *p = to_policy(pe);
 
-	return p->real_policy ? policy_residency(to_policy(pe)->real_policy) : 0;
+	return p->real_policy ? policy_residency(p->real_policy) : 0;
 }
 
 static void background_tick(struct dm_cache_policy *pe)
@@ -172,13 +197,17 @@ static int background_set_config_value(struct dm_cache_policy *pe,
 	struct policy *p = to_policy(pe);
 
 	if (!strcasecmp(key, clean_block_str)) {
-		unsigned long tmp;
+		if (!strcmp(value, "-1"))
+			p->threshold = p->cache_blocks;
+		else {
+			unsigned long tmp;
 
-		if (kstrtoul(value, 10, &tmp) ||
-		    tmp > from_cblock(p->cache_blocks))
-			return -EINVAL;
+			if (kstrtoul(value, 10, &tmp) ||
+			    tmp > from_cblock(p->cache_blocks))
+				return -EINVAL;
 
-		p->threshold = to_cblock(tmp);
+			p->threshold = to_cblock(tmp);
+		}
 
 	} else if (!strcasecmp(key, policy_str)) {
 		if (p->real_policy)
@@ -233,6 +262,8 @@ static struct dm_cache_policy *background_create(dm_cblock_t cache_blocks,
 
 	init_policy_functions(p, true);
 	p->threshold = 0;
+	atomic_set(&p->nr_dirty, 0);
+
 	p->cache_blocks = cache_blocks;
 
 	/* Save for set_config_value() stacked policy creation. */
